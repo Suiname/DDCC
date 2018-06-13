82a4be166cce942d763253edfc3031a5187e27d6
jtham <jtham@gogoair.com>
2018-06-13 11:37:59 -0500

    returning github data

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, Response, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify({'test': True})
+
+@app.route('/mash')
+def mash():
+    params = {
+        'bitbucket': request.args.get('bb_name'),
+        'github': request.args.get('gh_name')
+    }
+    url = 'https://api.github.com/users/{}/repos'.format(params['github'])
+    # print(url)
+    gh_req = requests.get(url)
+    gh_repos = gh_req.json()
+    return jsonify(gh_repos)
 
 
 if __name__ == "__main__":
