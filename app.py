e98c8967e93cd90205c0120b001fcd1ff0644762
jtham <jtham@gogoair.com>
2018-06-13 10:46:35 -0500

    test route working

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask, jsonify, Response, request
 
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({'test': True})
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
