3f63959713d8714d1b0888dbab59565c46135330
Jason Tham <jtham@sbcglobal.net>
2018-06-14 00:30:58 -0500

    merged working

--- app.py
+++ app.py
@@ -34,6 +34,52 @@ def create_response():
     return result
 
 
+def merge_bb_data(params, result):
+    bb_url = 'https://api.bitbucket.org/1.0/users/{}'.format(params['bitbucket'])
+    bb_req = requests.get(bb_url)
+    bb_repos = bb_req.json()
+    if bb_repos['repositories'] and len(bb_repos['repositories']):
+        for repo in bb_repos['repositories']:
+            slug = repo['slug']
+            if repo['is_fork']:
+                result['repo_count']['forked'] += 1
+            else:
+                result['repo_count']['original'] += 1
+            result['account_size'] += repo['size']
+            if(repo['language'] and
+               repo['language'].lower() not in result['languages']['list']
+               ):
+                result['languages']['list'].append(repo['language'].lower())
+                result['languages']['count'] += 1
+            repo_url = 'https://api.bitbucket.org/{}'.format(
+                repo['resource_uri'])
+            repo_req = requests.get(repo_url)
+            if repo_req.status_code == 200:
+                repo_data = repo_req.json()
+                result['repo_watchers'] += repo_data['followers_count']
+                if repo_data['has_issues']:
+                    issues_req = requests.get(repo_url + '/issues?status=open')
+                    issues_data = issues_req.json()
+                    result['open_issues'] += issues_data.get('count')
+              
+            follower_url = 'https://api.bitbucket.org/1.0/users/{}/followers'.format(
+                params['bitbucket']
+            )
+            follower_req = requests.get(follower_url)
+            if follower_req.status_code == 200:
+                follower_data = follower_req.json()
+                result['user_watchers'] += follower_data['count']
+            
+            commits_url = 'https://api.bitbucket.org/1.0/repositories/{}/{}/changesets/'.format(
+                params['bitbucket'], slug
+            )
+            commits_req = requests.get(commits_url)
+            if commits_req.status_code == 200:
+                commits_data = commits_req.json()
+                result['commits'] += commits_data.get('count')
+    return result
+
+
 @app.route('/test', methods=['GET'])
 def test():
     return jsonify({'test': True})
@@ -45,11 +91,10 @@ def mash():
         'bitbucket': request.args.get('bb_name'),
         'github': request.args.get('gh_name'),
     }
-    url = 'https://api.github.com/users/{}/repos?per_page=3'.format(
-        params['github']
-        )
-    # print(url)
     result = create_response()
+    url = 'https://api.github.com/users/{}/repos?per_page=3'.format(
+        params.get('github')
+        )
     gh_req = requests.get(url, auth=(username, password))
     gh_repos = gh_req.json()
     for repo in gh_repos:
@@ -63,19 +108,21 @@ def mash():
             result['open_issues'] += repo['open_issues_count']
             commit_url = (
                 'https://api.github.com/repos/{}/{}/contributors'
-                .format(params['github'], repo['name'])
+                .format(params.get('github'), repo['name'])
                 )
             commit_request = requests.get(commit_url, auth=(username, password))
             if commit_request.status_code == 200:
                 commits = commit_request.json()
-                user_commits = [x for x in commits if x['login'].lower() == params['github'].lower()]
+                user_commits = [x for x in commits if x['login'].lower() == params.get('github').lower()]
                 result['commits'] += user_commits[0]['contributions']
             result['account_size'] += repo['size']
-            if repo['language'] not in result['languages']['list']:
-                result['languages']['list'].append(repo['language'])
+            if(repo['language'] and
+               repo['language'].lower() not in result['languages']['list']
+               ):
+                result['languages']['list'].append(repo['language'].lower())
                 result['languages']['count'] += 1
             topics_url = 'https://api.github.com/repos/{}/{}/topics'.format(
-                params['github'], repo['name']
+                params.get('github'), repo['name']
             )
             print(topics_url)
             headers = {'Accept': "application/vnd.github.mercy-preview+json"}
@@ -86,6 +133,9 @@ def mash():
                 result['repo_topics']['count'] = len(result['repo_topics']['list'])
         else:  # malformed repo data
             raise 'Malformed Repository Data returned from Github'
+    bb_url = 'https://api.bitbucket.org/1.0/users/{}'.format(params['bitbucket'])
+    result = merge_bb_data(params, result)
+
     return jsonify(result)
 
 
